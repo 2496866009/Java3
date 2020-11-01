6ea646107fb24cc9731ccb28f230b937bb44417c
2496866009 <72741765+2496866009@users.noreply.github.com>
2020-11-02 00:40:29 +0800

    Add files via upload

---
+++ text.java
@@ -0,0 +1,11 @@
+package choose;
+
+public class text{
+	public static void main(String choose[]{
+		student stu1 = new student("1","张三","男","高数");
+		teacher tea1 = new teacher("1","李四","男","大物");
+		System.out.println("1","张三","男","高数");
+		System.out.println("1","李四","男","大物");
+	}
+}
+
